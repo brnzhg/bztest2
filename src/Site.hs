fee50d5b65e1717dab24fa8e1da78d8d44fd58f1
Brian Zhang <brnzhg@gmail.com>
2024-04-07 22:58:29 +0000

    cool

--- src/Site.hs
+++ src/Site.hs
@@ -1,18 +1,105 @@
 --------------------------------------------------------------------------------
-{-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+
+import           Control.Monad (foldM)
+import           Data.Maybe    (catMaybes, fromMaybe, isJust)
+import           Data.Monoid   (mappend)
+
 
 import           Hakyll
 
 
-cool :: Compiler ()
-cool = debugCompiler "yo" 
+newtype DomainName = DomainName String
+newtype DomainTgtKey = DomainTgtKey String
 
-bob :: IO ()
-bob = do
-    putStrLn "yo"
-    putStrLn "yoyo"
-    
+data DomainTarget = DomainTarget
+    { targetKey :: DomainTgtKey -- this is what's in the Markup
+    , target    :: Identifier
+    --, backLinks :: [Identifier]
+    }
+
+newtype DomainBacklinks = DomainBacklinks [(DomainTarget, [Identifier])]
+
+--TODO maybe rename target args?
+data DomainArgs = DomainArgs
+    { domainName          :: DomainName
+    , domainMakeTargetKey :: Identifier -> String -- note opposite of Tag pages, user decides location
+    }
+
+
+-- simple version, although could just register with filepath or register all
+-- replace MonadMetadata with more general MonadDomainTargetFilter
+buildDomainTargetsWith
+    :: MonadMetadata m
+    => DomainArgs
+    -> (Identifier -> m Bool)
+    -> Pattern
+    -> m [DomainTarget]
+buildDomainTargetsWith da targetFilter p = do
+    ids <- getMatches p
+    maybeTgts <- traverse getDefinedTgt ids
+    return . catMaybes $ maybeTgts
+    where
+        --getDefinedTgt :: Identifier -> m (Maybe DomainTarget)
+        getDefinedTgt identifier = do
+            filtered <- targetFilter identifier
+            let dtk = domainMakeTargetKey da identifier
+            return $ if filtered
+                then Just (DomainTarget (DomainTgtKey dtk) identifier)
+                else Nothing
+
+
+-- TODO in future need to parse through pandoc tree to find backlinks
+-- maybe use snaps for efficiency? not important for now
+-- just makiing this simple for now
+-- currently praying getRoute bypasses the circularity
+buildDomainBackLinksWith
+    :: forall m. MonadMetadata m
+    => DomainName
+    -> (Identifier -> m [DomainTgtKey])
+    -> [DomainTarget]
+    -> Pattern
+    -> m DomainBacklinks
+buildDomainBackLinksWith dn getLinkedTgts tgts p = do
+    ids <- getMatches p
+    foldM addLinkedTgts (DomainBacklinks []) ids
+    where
+        addLinkedTgts :: DomainBacklinks -> Identifier -> m DomainBacklinks
+        addLinkedTgts (DomainBacklinks lbt) identifier = do
+            linkedTgts <- getLinkedTgts identifier
+            return . DomainBacklinks
+                $ (\(dt, l)  -> (dt, if targetKey dt `elem` linkedTgts
+                            then identifier : l
+                            else l))
+                <$> lbt
+
+
+
+-- target-domains: ingredient
+-- files with this metadata are registered as ingredients
+simpleTargetFilter :: MonadMetadata m => Identifier -> DomainName -> m Bool
+simpleTargetFilter identifier dn = do
+    metadata <- getMetadata identifier
+    return . fromMaybe False $ do
+        ds <- lookupStringList "target-domains" metadata
+        return $ elem dn ds
+
+-- ingredient: apple, orange, pear
+-- a page's targets are under metadata field "domain"
+simpleGetLinkedTargets
+    :: MonadMetadata m
+    => DomainName -> Identifier -> m [DomainTgtKey]
+simpleGetLinkedTargets dn identifier = do
+    metadata <- getMetadata identifier
+    return . fromMaybe [] $ lookupStringList dn
+
+
+
+cool :: Compiler ()
+cool = debugCompiler "yo"
+
 
 main :: IO ()
 main = do
@@ -20,13 +107,8 @@ main = do
     putStrLn "sup"
 
 --------------------------------------------------------------------------------
--- register recipe (definition), register tags
-    -- need to make a link back to recipe (like section link)
-    -- ingredient tags? seems like good first pass
--- same thing with ingredients
 -- maybe later: register ingredients list for recipe (can have more than one and merge), each ingredient marked with special quotes?
     -- for now first list item marks recipe key
--- ingredients list, ingredient line in list can say whether to link to ingredients, tags like optional, alternatives, etc
 
 -- recipes page and ingredients page
     -- both use definition lists, plus alphabet links. Nav page at top for letter of alphabet
